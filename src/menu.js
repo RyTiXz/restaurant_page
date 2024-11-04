bc38bd8870f42489af59339a453cba827cf3d015
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-11-04 10:52:54 +0100

    Complete menu dishes, JS

--- src/menu.js
+++ src/menu.js
@@ -9,10 +9,39 @@ img.src= buffaloImage;
 img.alt="Picture of a buffalo"
 img.id="buffaloBanner"
 
+const menuHeader = document.createElement("h1")
+menuHeader.textContent = "Menu"
+
+const menuOne = document.createElement("h2")
+menuOne.textContent = "Lentil Bolognese"
+
+const menuOneDescription = document.createElement("p")
+menuOneDescription.className = "menuDescription"
+menuOneDescription.textContent = "A hearty, plant-based take on the Italian classic"
+
+const menuTwo = document.createElement("h2")
+menuTwo.textContent = "Butternut Squash Risotto"
+menuTwo
+const menuTwoDescription = document.createElement("p")
+menuTwoDescription.className = "menuDescription"
+menuTwoDescription.textContent = "A creamy, comforting dish"
+
+const menuThree = document.createElement("h2")
+menuThree.textContent = "Miso Tofu Wrap"
+
+const menuThreeDescription = document.createElement("p")
+menuThreeDescription.className = "menuDescription"
+menuThreeDescription.textContent = "A flavorful and filling wrap"
 
 
 // Exports
 export function menuScreen() {
-    contentSelector.appendChild(img)
-
+    contentSelector.appendChild(img);
+    contentSelector.appendChild(menuHeader);
+    menuHeader.appendChild(menuOne);
+    menuOne.appendChild(menuOneDescription);
+    menuHeader.appendChild(menuTwo);
+    menuTwo.appendChild(menuTwoDescription);
+    menuHeader.appendChild(menuThree);
+    menuThree.appendChild(menuThreeDescription);
 }
