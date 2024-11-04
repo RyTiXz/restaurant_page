86516bc888ef47116117c42c22653718de38bbd7
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-11-04 11:18:43 +0100

    Add about section, JS

--- src/about.js
+++ src/about.js
@@ -9,7 +9,31 @@ img.src= buffaloImage;
 img.alt="Picture of a buffalo"
 img.id="buffaloBanner"
 
+const aboutHeader = document.createElement("h1")
+aboutHeader.textContent = "Contact Us"
+
+const HeaderDiv = document.createElement("div")
+HeaderDiv.className = "contactDetails"
+
+const contactDetailsName = document.createElement("h2")
+contactDetailsName.textContent = "Grazing Ryan"
+
+const contactDetailsEmployment = document.createElement("p")
+contactDetailsEmployment.textContent = "Chef"
+
+const contactDetailsPhone = document.createElement("p")
+contactDetailsPhone.textContent = "+49 123 12345678"
+
+const contactDetailsMail = document.createElement("p")
+contactDetailsMail.textContent = "FakeMail@dontFallForIt.com"
+
 // Exports
 export function aboutScreen() {
     contentSelector.appendChild(img)
+    contentSelector.appendChild(aboutHeader)
+    contentSelector.appendChild(HeaderDiv)
+    HeaderDiv.appendChild(contactDetailsName)
+    HeaderDiv.appendChild(contactDetailsEmployment)
+    HeaderDiv.appendChild(contactDetailsPhone)
+    HeaderDiv.appendChild(contactDetailsMail)
 }
