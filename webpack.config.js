9277916bf8cce68e379dbea3cada00093897445e
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-11-04 06:42:49 +0100

    Change LocalHost, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
   },
   plugins: [
     new HtmlWebpackPlugin({
