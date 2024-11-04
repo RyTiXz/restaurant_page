3a6f6bb7795ecc10a33ac1191a0dd1eb0ee3b8db
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-11-04 10:34:18 +0100

    Add Script Files for restaurant pages, JS

---
+++ src/about.js
@@ -0,0 +1,15 @@
+// Querry Selector
+const contentSelector = document.querySelector("#content")
+
+// Imports
+import buffaloImage from "../img/buffaloBanner2.png"
+
+const img = document.createElement("img");
+img.src= buffaloImage;
+img.alt="Picture of a buffalo"
+img.id="buffaloBanner"
+
+// Exports
+export function aboutScreen() {
+    contentSelector.appendChild(img)
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import "./styles.css"
-import "./initialPage.js"
+import { homeScreen } from "./Home"
+import { menuScreen } from "./menu"
+import { aboutScreen } from "./about"
+
+const HomeButtonSelector = document.querySelector(".home")
+const menuButtonSelector = document.querySelector(".menu")
+const aboutButtonSelector = document.querySelector(".about")
+const contentSelector = document.querySelector("#content")
+
+function empty(element) {
+    while(element.firstElementChild) {
+        element.firstElementChild.remove();
+    }
+}
+
+HomeButtonSelector.addEventListener("click", () => {
+    empty(contentSelector)
+    homeScreen();
+});
+
+menuButtonSelector.addEventListener("click", () => {
+    empty(contentSelector)
+    menuScreen();
+});
+
+aboutButtonSelector.addEventListener("click", () => {
+    empty(contentSelector)
+    aboutScreen();
+});
+
+homeScreen();
